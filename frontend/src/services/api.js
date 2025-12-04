ce5fa031d2227221f0a295bb8359f75af6164faa
chelaniparth <parthprakashh@gmail.com>
2025-12-04 19:06:25 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://especial-jazmin-unstrangely.ngrok-free.dev',
     headers: {
         'Content-Type': 'application/json',
     },
